f15b6bfd3219ec0c3399a2367fbd6c05f488b245
NICOLAS TAVARES DE SOUZA <25254365@educ123.sp.senai.br>
2026-08-28 16:16:54 -0300

    atividades corretas

--- atividades/aula01/Atividade03/index.js
+++ atividades/aula01/Atividade03/index.js
@@ -1,8 +1,10 @@
 const nomeInformado = process.argv[2];
 const turmaInformada = process.argv[3];
 
-console.log('Entrada recebida:', { nomeInformado, turmaInformada });
-console.error('PENDENTE: valide nome e turma antes de confirmar a inscrição.');
-process.exitCode = 1;
+if (!nomeInformado || !turmaInformada){
+    console.error('Informe o nome e a turma');
+    process.exitCode=1;
+} else {
+    console.log({nomeInformado, turmaInformada});
+}
 
-// TODO: substitua as três linhas acima pela validação e pela confirmação pedidas.
