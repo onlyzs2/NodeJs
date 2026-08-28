865382a43b08e13dbf3f7f0fb165db283aafefc5
onlyzs2 <nic123hello@gmail.com>
2026-08-28 17:08:54 -0300

    aula02 copias

---
+++ exemplos/aula02/copias.js
@@ -0,0 +1,12 @@
+const original={
+    id: 1,
+    nome: 'Mouse',
+    estoque: 5
+}
+const atualizado= {...original, estoque:8};
+
+console.log({original, atualizado});
+
+const {nome, estoque} = atualizado;
+console.log(`${nome} possui ${estoque} unidade(s)`);
+// descomponentizacao//
